81d3017aa4b8bb58542ab023c679fefcb74fadac
mahmudul hasan <mahmudulhasandigonto@gmail.com>
2022-10-13 15:09:26 -0700

    After Complite Desing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Detail from './components/Detail';
+import Contact from './components/Contact';
+import Checkout from './components/Checkout';
+import Cart from './components/Cart';
+import Shop from './components/Shop';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+
+
+        <Route path="/" element={<Home />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/shop" element={<Shop />} />
+
+
+
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
